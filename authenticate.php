a3226e68e704d84965f05fcab50025e5d832e836
Jack Kwaak <JKwaak@americanoverwatch.org>
2021-02-07 22:12:54 -0500

    changed database host

--- authenticate.php
+++ authenticate.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 // Change this to your connection info.
-$DATABASE_HOST = 'localhost';
+$DATABASE_HOST = 'flpb01';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
 $DATABASE_NAME = 'phplogin';
